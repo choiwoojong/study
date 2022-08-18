3f122969757d7daf8820a690168a224fa22f690f
woojong choi <choiwoojong232@gmail.com>
2022-08-18 12:37:05 +0900

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const data = prompt("아무거나");
+let aaa = 5;
+console.log(aaa);
+const bb = 3;
+console.log(bb)
+const promptMessage = "프롬프트에 표시할 문자열" 
+const result = prompt(promptMessage)
+console.log(result)
