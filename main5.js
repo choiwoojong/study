69ac2ab7537656c5608336d2f9f1ed2299100d84
woojong choi <choiwoojong232@gmail.com>
2022-09-28 10:59:36 +0900

    clone coding

--- main5.js
+++ main5.js
@@ -68,3 +68,64 @@ function printFruitObj(obj){
   console.log(obj.name +"은 "+ obj.price +"원 입니다.");
 }
 printFruitObj({name:'apple', price:3000});
+
+function rockPaperScissors(player1, player2) {
+  if ( (player1 === "가위" && player2 === "보") || (player1 === "가위" && player2 === "가위") ) {
+    return "player1";
+  }
+} 
+
+function checkYear() {
+  let year = 2022
+  return year;
+}
+
+console.log(checkYear)
+
+function addTen(num) {
+  return num + 10;
+
+}
+
+function addTen(num) {
+  return num + 10; 
+}
+
+function isBiggerThanHundred(myNumber) {
+  if (myNumber > 100); {
+    return "크다" {
+  } else if (myNumber <= 100) {
+    return "작다!"
+  }
+}
+
+
+let cities = ["서울", "대전", "대구", "부산", "광주", "제주도"];
+
+//만약 home 이 "서울"이면, "아, 여기 사시는군요" 라고 alert를 띄우고 싶다.
+
+let home = "서울";
+
+if (cities[0] === home) {
+  alert("아, 여기 사시는군요");
+} else if (cities[1] === home) {
+  alert("아, 여기 사시는군요"); {}
+}
+
+let cities = [];
+
+cities[0] = "서울";
+cities[1] = "대전";
+cities[2] = "대구";
+
+console.log(cities)
+
+const id = prompt("아이디를 입력해 주세요.");
+  if(id === "tong232") {
+  const pw2 = prompt("비밀번호를 입력해 주세요.");
+    if(pw2 === "tttttt") {
+        alert("환영합니다.");
+    } else {
+      alert("비밀번호를 확인해 주세요.");
+    }
+  }
